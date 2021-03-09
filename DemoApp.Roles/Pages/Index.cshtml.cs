b14c9e9c2bb147e694b5be5b159e4dedd4cf6c42
Frode Hus <frode.hus@outlook.com>
2021-03-09 11:26:12 +0100

    finished sample (#1)

---
+++ DemoApp.Roles/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace DemoApp.Roles.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["roles"] = HttpContext.User.Claims.Where(p => p.Type == ClaimTypes.Role).Aggregate(string.Empty, (s, next) => s + next.Value + ",");
+
+        }
+    }
+}
